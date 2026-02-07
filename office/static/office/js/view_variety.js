7f5be45a03de9eab270c7c92a9e7cb4e92329ee6
Jesse <jesse.wimer@wsu.edu>
2026-02-06 19:31:48 -0800

    Remove veg type in variety search matching

--- office/static/office/js/view_variety.js
+++ office/static/office/js/view_variety.js
@@ -181,21 +181,10 @@ function setupSearch() {
             const matchesCommonSpelling = data.common_spelling && data.common_spelling.toLowerCase().includes(query);
             const matchesCrop = data.crop && data.crop.toLowerCase().includes(query);
             
-            if (matchesCommonSpelling || matchesCrop || matchesVegType) {
+            if (matchesCommonSpelling || matchesCrop) {
                 matches.push([skuPrefix, data]);
             }
         }
-        // // Only search through the common_spelling keys
-        // const matches = [];
-        
-        // for (const [skuPrefix, data] of Object.entries(allVarieties)) {
-        //     const matchesCommonSpelling = data.common_spelling && data.common_spelling.toLowerCase().includes(query);
-            
-        //     if (matchesCommonSpelling) {
-        //         matches.push([skuPrefix, data]);
-        //     }
-        // }
-        // console.log('Found matches:', matches.length);
 
         if (matches.length > 0) {
 
